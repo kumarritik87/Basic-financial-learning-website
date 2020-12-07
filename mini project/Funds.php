2b7d4d36cb9a4b3ac9921874a1a97c252c91a438
rishikasingh7055 <rishika.singh_cs18@gla.ac.in>
2020-12-07 12:12:45 +0530

    final commit

--- mini project/Funds.php
+++ mini project/Funds.php
@@ -6,45 +6,53 @@
     <title>Best stocks content</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+<style>
+div.background{
+    background-image:url('funds.jpg');
+    
+}
+
+</style>
 <body>
  <div class="box">
-                    <img src="funds.jpg" alt="">
+                    <div class='background'>
                     <h1 class="h-secondary centre">Funds</h1>
 
-                    <h1 class="h-secondary centre">Steps To Find Best Mutual Funds</h1>
-                    <h2> 1) check over 10 year performance of funds </h2>
-                    <h2> 2) for this you can visit to www.valueresearch.com </h2>
-                    <h2> 3) you can also use Groww investment app </h2>
-                    <h2> 4) check the expense ratio of funds lesser ratio better the returns </h2>
-                    <h2> 5) on Groww app you can also read pros and cons of that fund</h2>
+                    <h1 style="color:red;" class="h-secondary centre">Steps To Find Best Mutual Funds</h1>
+                    <h2  style="color:blue;" > 1) check over 10 year performance of funds </h2>
+                    <h2  style="color:blue;" > 2) for this you can visit to <a href="https://www.valueresearchonline.com/#welcome-vr-portfolio">Valuesearch</a> </h2>
+                    <h2  style="color:blue;" > 3) you can also use <a href="https://groww.in/">Groww</a> investment app </h2>
+                    <h2  style="color:blue;" > 4) check the expense ratio of funds lesser ratio better the returns </h2>
+                    <h2  style="color:blue;" > 5) on Groww app you can also read pros and cons of that fund</h2>
 
 
 
-                    <h1 class="h-secondary centre">Active Funds</h1>
+                    <h1  style="color:red;"  class="h-secondary centre">Active Funds</h1>
 
 
-                    <h2> active funds are the type of mutual funds in which mutual fund houses hire the Expert and all your money will be in hands of an expert which gives you better result</h2>
+                    <h2 style="color:blue;" > active funds are the type of mutual funds in which mutual fund houses hire the Expert and all your money will be in hands of an expert which gives you better result</h2>
 
 
-                    <h1 class="h-secondary centre">Index Funds</h1>
+                    <h1 style="color:red;"  class="h-secondary centre">Index Funds</h1>
 
 
-                    <h2> index are usualy depends on growth of indices (sensex,nifty) in index funds expense ratio is very less and they can beat about 70 active funds over the years</h2>
+                    <h2 style="color:blue;" > index are usualy depends on growth of indices (<a href="https://www.moneycontrol.com/indian-indices/sensex-4.html">sensex</a>,<a href="https://www1.nseindia.com/products/content/equities/indices/historical_pepb.htm">nifty</a>) in index funds expense ratio is very less and they can beat about 70 active funds over the years</h2>
                     
 
 
 
-                    <h1 class="h-secondary centre"> our recomandation for 2021</h1>
+                    <h1 style="color:red;"  class="h-secondary centre"> our recomandation for 2021</h1>
 
-                    <h1 class="h-secondary centre"> Active funds for 2021</h1>
+                    <h1 style="color:red;"  class="h-secondary centre"> Active funds for 2021</h1>
 
 
-                     <h2> parag parikh long term equity fund<h2>
+                     <h2 style="color:blue;" > parag parikh long term equity fund <a href="https://amc.ppfas.com/">click</a><h2>
                     
 
-                    <h1 class="h-secondary centre"> Index fund for 2021</h1> 
-                    <h2> HDFC Index sensex direct plan growth </h2>
+                    <h1 style="color:red;"  class="h-secondary centre"> Index fund for 2021</h1> 
+                    <h2 style="color:blue;" > HDFC Index sensex direct plan growth <a href="https://www.valueresearchonline.com/funds/16071/hdfc-index-fund-sensex-plan-direct-plan">click</a> </h2>
 
                      </div> 
+                     </div>
 </body>
 </html>
--- mini project/Services.php
+++ mini project/Services.php
@@ -7,36 +7,45 @@
     <link rel="stylesheet" href="css/style.css">
 
 </head>
+<style>
+div.background{
+    background-image:url('background.jpg');
+    
+}
+</style>
 <body>
 
-
+            <div class="background">
                  <div class="box">
-                <img src="background.jpg" alt="">
-                <h1 class="h-secondary centre">Share Market</h1>
-
+                <br>
+                <h1  class="h-secondary centre"><b>  Share Market  </b></h1>
+                <br><br><br><br><br><br><br><br>
 
                 <h2> For investing in stocks first you need to learn Fundamental analysis</h2>
-
+                 <br><br>
                 <h1> Best books for learning stock market</h1>
-
-                <h2> 1) Learn to Earn by Peter Lynch</h2>
-                <h2> 2)Investonomy by pranjal kamra</h2>
-                <h2> 3)The intelligent investor</h2>  
-
-                <h1> To Open Deemat Account</h1>
-                <h2> Zerodha </h2>
-                <h2> Upstock </h2>
-                <h2> Groww </h2> 
-
+                <br>
+                <h2> 1) Learn to Earn by Peter Lynch<a href="https://www.amazon.in/Learn-Earn-Beginners-Investing-Business/dp/0684811634/ref=sr_1_1?dchild=1&keywords=Learn+to+Earn+by+Peter+Lynch&qid=1606287614&sr=8-1">to buy click</a></h2>
+                <h2> 2)Investonomy by pranjal kamra<a href="https://www.amazon.in/Investonomy-Stock-Market-Guide-Makes/dp/1647607272/ref=sr_1_1?crid=2DXFOXXV9P410&dchild=1&keywords=investonomy+by+pranjal+kamra&qid=1606287697&sprefix=invest%2Caps%2C550&sr=8-1">to buy click</a></h2>
+                <h2> 3)The intelligent investor<a href="https://www.amazon.in/Intelligent-Investor-English-Paperback-2013/dp/0062312685/ref=sr_1_1?crid=3BE0GEUZO2S83&dchild=1&keywords=the+intelligent+investor&qid=1606287768&sprefix=the+inte%2Caps%2C385&sr=8-1">to buy click</a></h2>  
+                <br><br><br>
+                <h1> To Open Demat Account</h1>
+                <br>
+                <h2><a href="https://zerodha.com/">Zerodha</a></h2>
+                <h2><a href="https://upstox.com/">Upstox</a></h2>
+                <h2><a href="https://groww.in/">Groww</a></h2> 
+                <br><br><br>
                 <h1> Best online playlist for Fundamental analysis</h1>
-                <h2> Pranjal kamra series of Stock market</h2>  
-
-                <h1> Best tool for analysis of company </h1>
-                <h2> Ticker by finoloogy </h2>
-                <h2> Moneycontrol</h2>
-
+                <br>
+                <h2> Pranjal kamra series of Stock market<a href="https://www.youtube.com/watch?v=3UF0ymVdYLA&list=PLFQ0hRWyH11RS4KUPadj6aoC2KtEHVIfN">click</a></h2>  
+                <br><br><br>
+                <h1> Best tool for stock analysis of company </h1>
+                <br>
+                <h2><a href="https://ticker.finology.in/">Ticker</a>by finoloogy </h2>
+                <h2> <a href="https://www.moneycontrol.com/">Moneycontrol</a></h2>
+                <br><br><br>
                 <h1> Some Tips</h1>
-
+                <br>
                 <h2> Always invest in stocks when Market PE is in low and Moderate condition </h2>
                 <h2> Low PE = 10-15 </h2>
                 <h2> Moderate PE = 15-25</h2>
@@ -46,7 +55,7 @@
 
                 
                 </div>
-
+            </div>
                 
 </body>
 </html>
